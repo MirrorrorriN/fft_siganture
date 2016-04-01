dc12894b292deca73e5f84829b8472df35044aa0
somunsfool <somunsfool@163.com>
2016-04-01 15:29:45 +0800

    Update view_data.m

--- view_data.m
+++ view_data.m
@@ -3,16 +3,16 @@
 %32 bit chars map to one float,64 bit chars map to one complex
 function dr=view_data(filename,start,len)  
     if(nargin<1) 
-        filename='test_rx.dat';
+        filename='mpsk_04.dat';
     end
     if (nargin<3) %optional parameter
         start=0;
         len=Inf;
     end
     cr=read_complex_binary(filename,start,len);
-    ar=real(cr);
-    br=real(cr);
-    lr=sqrt(ar.^2+br.^2);
+    %ar=real(cr);
+    %br=imag(cr);
+    lr=abs(cr);
     dr=20*log10(lr);
     
 %     ct=read_complex_binary(filename,start,len);
