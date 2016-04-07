a4de14431fbc8bf6f058b8ebb72ea45e3f9d5afc
somunsfool <somunsfool@163.com>
2016-04-07 21:59:30 +0800

    Update mpsk_wo_demod.m

--- mpsk_wo_demod.m
+++ mpsk_wo_demod.m
@@ -1,21 +1,21 @@
 %searching for the feature of wave transfer by mpsk modification
-filename='4_6/USRP_04/mpsk_wo_demod4.dat';
+filename='USRP_04/mpsk_wo_demod.dat';
 start=0*8;  %every 8 bytes represent one complex data
 group=2000;
 subgroup=100;
 fft_size=4096;   %fft size
-peaknum=2;
+peaknum=5;
 [p fr]=peak_hold(filename,start,group,subgroup,fft_size);
-pri=[1054,3102];
+pri=[511,1054,2048,3102,3478];
 
 big=zeros(subgroup,peaknum);
 tag=zeros(subgroup,peaknum);
 for i=1:1:subgroup
     temp=p(i,:);
     for j=1:1:peaknum
-        [big(i,j),tag(i,j)]=max(temp(pri(j)-20*fft_size/1024:pri(j)+20*fft_size/1024));
+        [big(i,j),tag(i,j)]=max(temp(pri(j)-5*fft_size/1024:pri(j)+5*fft_size/1024));
         %temp(tag(i,j)-10*fft_size/1024:tag(i,j)+10*fft_size/1024)=min(temp);
-        tag(i,j)=tag(i,j)+pri(j)-20*fft_size/1024-1
+        tag(i,j)=tag(i,j)+pri(j)-5*fft_size/1024-1
     end
 end
 
